65265e32c34d108e9f795123f8bf9f287897e296
Rusaid MRD <rusaidmrd@gmail.com>
2020-03-04 15:09:35 +0300

    Completing Settings Section

---
+++ app/Http/Controllers/Admin/SettingController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\BaseController;
+use App\Traits\UploadAble;
+use App\Setting;
+use Illuminate\Http\UploadedFile;
+
+class SettingController extends BaseController
+{
+    use UploadAble;
+
+    public function index()
+    {
+        $this->setPageTitle('Settings', 'Manage Settings');
+        return view('admin.settings.index');
+    }
+
+    public function update(Request $request)
+    {
+        if ($request->has('site_logo') && ($request->file('site_logo') instanceof UploadedFile)) {
+
+            if (config('settings.site_logo') != null) {
+                // In here config function is retreving the site_logo value from settings table
+                // which is globaly available done by SettingServiceProvider
+                $this->deleteOne(config('settings.site_logo'));
+            }
+            $logo = $this->uploadOne($request->file('site_logo'), 'img');
+            Setting::set('site_logo', $logo);
+
+        } elseif ($request->has('site_favicon') && ($request->file('site_favicon') instanceof UploadedFile)) {
+
+            if (config('settings.site_favicon') != null) {
+                $this->deleteOne(config('settings.site_favicon'));
+            }
+            $favicon = $this->uploadOne($request->file('site_favicon'), 'img');
+            Setting::set('site_favicon', $favicon);
+
+        } else {
+
+            $keys = $request->except('_token');
+
+            foreach ($keys as $key => $value)
+            {
+                Setting::set($key, $value);
+            }
+        }
+        return $this->responseRedirectBack('Settings updated successfully.', 'success');
+    }
+}
--- routes/admin.php
+++ routes/admin.php
@@ -13,6 +13,9 @@ Route::group(['prefix'  =>  'admin'], function () {
             return view('admin.dashboard.index');
         })->name('admin.dashboard');
 
+        Route::get('settings', 'Admin\SettingController@index')->name('admin.settings');
+        Route::post('settings', 'Admin\SettingController@update')->name('admin.settings.update');
+
     });
 
 
