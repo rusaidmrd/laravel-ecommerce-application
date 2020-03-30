8a93f43fc09f7baa0820deb238ae67dda633bb91
Rusaid MRD <rusaidmrd@gmail.com>
2020-03-30 09:56:46 +0300

    Completing the checkout page

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -3,14 +3,21 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+
 use App\Contracts\CategoryContract;
 use App\Repositories\CategoryRepository;
+
 use App\Contracts\AttributeContract;
 use App\Repositories\AttributeRepository;
+
 use App\Contracts\BrandContract;
 use App\Repositories\BrandRepository;
+
 use App\Contracts\ProductContract;
 use App\Repositories\ProductRepository;
+
+use App\Contracts\OrderContract;
+use App\Repositories\OrderRepository;
 
 class RepositoryServiceProvider extends ServiceProvider
 {
@@ -20,6 +27,7 @@ class RepositoryServiceProvider extends ServiceProvider
         AttributeContract::class => AttributeRepository::class,
         BrandContract::class =>  BrandRepository::class,
         ProductContract::class => ProductRepository::class,
+        OrderContract::class => OrderRepository::class,
     ];
 
     /**
